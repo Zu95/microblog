c39ee44e481ab369bcc63063f12a099df335e715
Zu95 <zkrzysz@gmail.com>
2018-05-06 15:25:52 +0200

    basic functions work

--- ClimbingABC/urls.py
+++ ClimbingABC/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('/', views.index, name='index'),
     path('', views.index, name='index'),
-    path('category/', views.index, name='index'),
     path('category/<int:category_id>', views.category, name='category'),
-    path('category/<int:category_id>/post/<int:post_id>', views.category, name='category'),
+    path('category/<int:category_id>/article/<int:post_id>', views.article, name='article'),
 ]
--- ClimbingABC/views.py
+++ ClimbingABC/views.py
@@ -7,11 +7,26 @@ from ClimbingABC.models import Post
 
 def index(request):
     categories = Category.objects.all()
-    return render(request, 'index.html', {'categories': categories})
+    newest_posts = Post.objects.order_by('-published_date')[:3]
+    gallery = Post.objects.order_by('?')[:4]
+    return render(request, 'index.html', {'categories': categories,
+                                          'newest_posts': newest_posts,
+                                          'gallery': gallery})
 
 
 def category(request, category_id):
     categories = Category.objects.all()
     current_category = categories.get(id=category_id)
     posts = Post.objects.all().filter(category=category_id)
-    return render(request, 'category.html', {'current_category': current_category, 'posts': posts, 'categories': categories})
+    return render(request, 'category.html', {'current_category': current_category,
+                                             'posts': posts,
+                                             'categories': categories})
+
+
+def article(request, category_id, post_id):
+    categories = Category.objects.all()
+    current_category = categories.get(id=category_id)
+    post = Post.objects.get(id=post_id)
+    return render(request, 'article.html', {'categories': categories,
+                                            'current_category': current_category,
+                                            'post': post})
