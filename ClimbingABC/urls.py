23f2786d3c6364d9c32058efb0b87a55da3f659d
Zu95 <zkrzysz@gmail.com>
2018-06-14 16:15:36 +0200

    staticfiles work

--- ClimbingABC/urls.py
+++ ClimbingABC/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name='index'),
     path('', views.index, name='index'),
     path('category/<int:category_id>', views.category, name='category'),
     path('category/<int:category_id>/article/<int:post_id>', views.article, name='article'),
--- Licencjat/urls.py
+++ Licencjat/urls.py
@@ -18,11 +18,13 @@ from django.conf.urls import include
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import Licencjat
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ClimbingABC.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += static(Licencjat.settings.STATIC_URL, document_root=settings.STATIC_ROOT)
